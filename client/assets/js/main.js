7efecad89b960eefc7ce3d6dbbf3d86bfaaa7b40
(Hai Nguyen).Net <hainvk53@gmail.com>
2023-02-17 11:03:46 +0700

    update api

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch('https://chatgpy.onrender.com', {
+  const response = await fetch('https://chatgpy-bhtg.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
